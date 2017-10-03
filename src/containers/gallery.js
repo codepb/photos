65416237c4267606f950b6b8dda63e96a564cde9
codepb <paul.j.burgess@outlook.com>
2017-10-03 22:28:53 +0100

    gallery now works from total number of images

--- src/containers/gallery.js
+++ src/containers/gallery.js
@@ -3,7 +3,10 @@ import Gallery from '../components/gallery';
 
 const images = [
     {src:'/photos/Bassenthwaite (1 of 2).jpg', caption: {title: 'Bassenthwaite', content: 'Trees lined up against the shore.'}},
-    {src: '/photos/Bassenthwaite (2 of 2).jpg', caption: {title: 'Bassenthwaite and Hills', content: 'Rocks with hills behind.'}}
+    {src: '/photos/Bassenthwaite (2 of 2).jpg', caption: {title: 'Bassenthwaite and Hills', content: 'Rocks with hills behind.'}},
+    {src: '/photos/Bassenthwaite-3.jpg', caption: {title: 'Bassenthwaite hills', content: 'Low clouds on the hills.'}},
+    {src: '/photos/Bridge-1.jpg', caption: {title: 'Framwellgate Bridge', content: 'Underside of Framwellgate Bridge.'}},
+    {src: '/photos/Gibsons Cave-1.jpg', caption: {title: 'Gibson\'s Cave', content: 'Waterfall at Gibson\'s Cave.'}},
 ]
 
 export default class GalleryContainer extends React.Component {
@@ -15,7 +18,7 @@ export default class GalleryContainer extends React.Component {
     }
 
     nextImage = () => {
-        this.setState((state) => {return {currentImageIndex: (state.currentImageIndex + 1) % 2}})
+        this.setState((state) => {return {currentImageIndex: (state.currentImageIndex + 1) % images.length}})
     }
 
     render() {
