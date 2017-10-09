d3760103d43ae62c4d77be7caaa328b6c7ee2dc5
codepb <paul.j.burgess@outlook.com>
2017-10-09 12:31:50 +0100

    added routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'react-jss';
 import './index.css';
-import App from './App';
+import { routes } from './routes';
 import registerServiceWorker from './registerServiceWorker';
 import { colors } from './theme';
 
-ReactDOM.render(<ThemeProvider theme={{ colors: colors }}><App /></ThemeProvider>, document.getElementById('root'));
+ReactDOM.render(
+    <ThemeProvider theme={{ colors: colors }}>
+        <BrowserRouter children={ routes } />
+    </ThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,11 +2,12 @@ import * as React from 'react';
 import CenterChildren from '../components/centerChildren';
 
 const Home = () => ([
-    <h1 key="title" style={{color: '#000', fontFamily: 'raleway', fontWeight: 300, fontSize: 34, textAlign: 'left', textTransform: 'uppercase', marginLeft: 20}}>Paul Burgess Photography</h1>,
-    <CenterChildren key="image">
+    <h1 key="title" style={{color: '#000', fontFamily: 'raleway', fontWeight: 300, fontSize: 24, textAlign: 'left', textTransform: 'uppercase', marginLeft: 40}}>Paul Burgess Photography</h1>,
+    <nav key="menu"></nav>,
+    <CenterChildren key="image" style={{width: 'calc(100vw - 80px)', height: 'calc(100vh - 92px)', position: 'relative', marginLeft: 40, marginBottom: 30}}>
         <div style={{
-            width: '90vw',
-            height: '80vh',
+            width: '100%',
+            height: '100%',
             background: 'url("/photos/Gibsons Cave-1.jpg") no-repeat center center fixed',
             backgroundSize: 'cover'
             }}></div>
