ddb4ffdcc786bb6b80024f4aafeebaf1ab20f860
codepb <paul.j.burgess@outlook.com>
2017-11-05 12:55:30 +0000

    updating photo order

--- src/photos.js
+++ src/photos.js
@@ -4,11 +4,6 @@ const photos = [
         '/photos/Tranquility-1024.jpg 1024w',
         '/photos/Tranquility-512.jpg 512w'
     ], width: 5544, height: 3696, caption: {title: 'Tranquility', content: 'The sea reflects the meaning of tranquility. Nothing quite matches the sound of the sea as the sun sets.'}},
-    {src: '/photos/GibsonsCave.jpg', srcSet: [
-        '/photos/GibsonsCave-2048.jpg 2048w',
-        '/photos/GibsonsCave-1024.jpg 1024w',
-        '/photos/GibsonsCave-512.jpg 512w'
-    ], width: 6288, height: 4197, caption: {title: 'Gibson\'s Cave', content: 'Waterfall at Gibson\'s Cave.'}},
     {src: '/photos/Ghostly.jpg', srcSet: [
         '/photos/Ghostly-2048.jpg 2048w',
         '/photos/Ghostly-1024.jpg 1024w',
@@ -19,42 +14,35 @@ const photos = [
         '/photos/Bassenthwaite-1024.jpg 1024w',
         '/photos/Bassenthwaite-512.jpg 512w'
     ], width: 7360, height: 3969, caption: {title: 'Bassenthwaite', content: 'Trees lined up against the shore.'}},
-    {src: '/photos/HighForce.jpg', srcSet: [
-        '/photos/HighForce-2048.jpg 2048w',
-        '/photos/HighForce-1024.jpg 1024w',
-        '/photos/HighForce-512.jpg 512w'
-    ], width: 5807, height: 4646,
-    caption: {title: 'High Force', content: 'Afternoon at High Force.'}},
-    {src: '/photos/HighForce2.jpg', srcSet: [
-        '/photos/HighForce2-2048.jpg 2048w',
-        '/photos/HighForce2-1024.jpg 1024w',
-        '/photos/HighForce2-512.jpg 512w'
-    ], width: 7360, height: 4912,
-    caption: {title: 'High Force', content: 'Afternoon at High Force.'}},
-    {src: '/photos/TeesValley.jpg', srcSet: [
-        '/photos/TeesValley-2048.jpg 2048w',
-        '/photos/TeesValley-1024.jpg 1024w',
-        '/photos/TeesValley-512.jpg 512w'
-    ], width: 6140, height: 4912,
-    caption: {title: 'Tees Valley', content: 'Evening over Tees Valley.'}},
-    {src: '/photos/BelmontViaductBW.jpg', srcSet: [
-        '/photos/BelmontViaductBW-2048.jpg 2048w',
-        '/photos/BelmontViaductBW-1024.jpg 1024w',
-        '/photos/BelmontViaductBW-512.jpg 512w'
-    ], width: 4358, height: 5447,
-    caption: {title: 'Belmont Viaduct', content: 'This magnificent viaduct towers over the river weir.'}},
     {src: '/photos/BelmontViaduct.jpg', srcSet: [
         '/photos/BelmontViaduct-2048.jpg 2048w',
         '/photos/BelmontViaduct-1024.jpg 1024w',
         '/photos/BelmontViaduct-512.jpg 512w'
     ], width: 7360, height: 4912,
     caption: {title: 'Belmont Viaduct', content: 'This magnificent viaduct towers over the river weir.'}},
-    {src: '/photos/NorwichCathedral.jpg', srcSet: [
-        '/photos/NorwichCathedral-2048.jpg 2048w',
-        '/photos/NorwichCathedral-1024.jpg 1024w',
-        '/photos/NorwichCathedral-512.jpg 512w'
-    ], width: 4824, height: 3216,
-    caption: {title: 'Norwich Cathedral', content: 'Cathedral\'s have the most magnificent architecture. I always love to look up and see the detail that goes into the ceilings, that often goes unseen.'}},
+    {src: '/photos/HighForce2.jpg', srcSet: [
+        '/photos/HighForce2-2048.jpg 2048w',
+        '/photos/HighForce2-1024.jpg 1024w',
+        '/photos/HighForce2-512.jpg 512w'
+    ], width: 7360, height: 4912,
+    caption: {title: 'High Force', content: 'Afternoon at High Force.'}},
+    {src: '/photos/LowForce.jpg', srcSet: [
+        '/photos/LowForce-2048.jpg 2048w',
+        '/photos/LowForce-1024.jpg 1024w',
+        '/photos/LowForce-512.jpg 512w'
+    ], width: 7360, height: 4912,
+    caption: {title: 'Low Force', content: 'The waterfalls at low force.'}},
+    {src: '/photos/GibsonsCave.jpg', srcSet: [
+        '/photos/GibsonsCave-2048.jpg 2048w',
+        '/photos/GibsonsCave-1024.jpg 1024w',
+        '/photos/GibsonsCave-512.jpg 512w'
+    ], width: 6288, height: 4197, caption: {title: 'Gibson\'s Cave', content: 'Waterfall at Gibson\'s Cave.'}},
+    {src: '/photos/TeesValley.jpg', srcSet: [
+        '/photos/TeesValley-2048.jpg 2048w',
+        '/photos/TeesValley-1024.jpg 1024w',
+        '/photos/TeesValley-512.jpg 512w'
+    ], width: 6140, height: 4912,
+    caption: {title: 'Tees Valley', content: 'Evening over Tees Valley.'}},
     {src: '/photos/Tree.jpg', srcSet: [
         '/photos/Tree-2048.jpg 2048w',
         '/photos/Tree-1024.jpg 1024w',
@@ -67,18 +55,18 @@ const photos = [
         '/photos/HayBale-512.jpg 512w'
     ], width: 5034, height: 2388,
     caption: {title: 'Hay Bale', content: 'Walking through the fields, the hay bales add to the scene. The shapes of the hay bales are a perfect contrast to the lines of the fields.'}},
-    {src: '/photos/LowForce.jpg', srcSet: [
-        '/photos/LowForce-2048.jpg 2048w',
-        '/photos/LowForce-1024.jpg 1024w',
-        '/photos/LowForce-512.jpg 512w'
-    ], width: 7360, height: 4912,
-    caption: {title: 'Low Force', content: 'The waterfalls at low force.'}},
     {src: '/photos/Fields.jpg', srcSet: [
         '/photos/Fields-2048.jpg 2048w',
         '/photos/Fields-1024.jpg 1024w',
         '/photos/Fields-512.jpg 512w'
     ], width: 3771, height: 1964,
-    caption: {title: 'Fields', content: 'Something about the fields.'}}
+    caption: {title: 'Fields', content: 'Something about the fields.'}},
+    {src: '/photos/NorwichCathedral.jpg', srcSet: [
+        '/photos/NorwichCathedral-2048.jpg 2048w',
+        '/photos/NorwichCathedral-1024.jpg 1024w',
+        '/photos/NorwichCathedral-512.jpg 512w'
+    ], width: 4824, height: 3216,
+    caption: {title: 'Norwich Cathedral', content: 'Cathedral\'s have the most magnificent architecture. I always love to look up and see the detail that goes into the ceilings, that often goes unseen.'}}
 ];
 
 export default photos;
