c3543c805e35e02b4666e9e70e3fe3f9fb2f432b
codepb <paul.j.burgess@outlook.com>
2017-11-18 13:54:30 +0000

    formatting photos.js file better

--- src/photos.js
+++ src/photos.js
@@ -1,72 +1,172 @@
 const photos = [
-    {src: '/photos/Tranquility.jpg', srcSet: [
-        '/photos/Tranquility-2048.jpg 2048w',
-        '/photos/Tranquility-1024.jpg 1024w',
-        '/photos/Tranquility-512.jpg 512w'
-    ], width: 5544, height: 3696, caption: {title: 'Tranquility', content: 'When the sun sets over the sea, the world is so peaceful. The colours reflect the peacefulness.'}},
-    {src: '/photos/Ghostly.jpg', srcSet: [
-        '/photos/Ghostly-2048.jpg 2048w',
-        '/photos/Ghostly-1024.jpg 1024w',
-        '/photos/Ghostly-512.jpg 512w'
-    ], width: 1809, height: 2171, caption: {title: 'Ghostly Pillars', content: '.'}},
-    {src:'/photos/Bassenthwaite.jpg', srcSet: [
-        '/photos/Bassenthwaite-2048.jpg 2048w',
-        '/photos/Bassenthwaite-1024.jpg 1024w',
-        '/photos/Bassenthwaite-512.jpg 512w'
-    ], width: 7360, height: 3969, caption: {title: 'Bassenthwaite', content: '.'}},
-    {src: '/photos/BelmontViaduct.jpg', srcSet: [
-        '/photos/BelmontViaduct-2048.jpg 2048w',
-        '/photos/BelmontViaduct-1024.jpg 1024w',
-        '/photos/BelmontViaduct-512.jpg 512w'
-    ], width: 7360, height: 4912,
-    caption: {title: 'Belmont Viaduct', content: 'This magnificent viaduct towers over the river weir. In the middle of the natural landscape, the man made monument compliments it well.'}},
-    {src: '/photos/HighForce2.jpg', srcSet: [
-        '/photos/HighForce2-2048.jpg 2048w',
-        '/photos/HighForce2-1024.jpg 1024w',
-        '/photos/HighForce2-512.jpg 512w'
-    ], width: 7360, height: 4912,
-    caption: {title: 'High Force', content: 'The power of High Force is always awe-inspiring. When you freeze it to just a moment, the movement of the water looks so graceful, and far from the loud, rushing waterfall.'}},
-    {src: '/photos/LowForce.jpg', srcSet: [
-        '/photos/LowForce-2048.jpg 2048w',
-        '/photos/LowForce-1024.jpg 1024w',
-        '/photos/LowForce-512.jpg 512w'
-    ], width: 7360, height: 4912,
-    caption: {title: 'Low Force', content: 'The waterfalls at low force.'}},
-    {src: '/photos/GibsonsCave.jpg', srcSet: [
-        '/photos/GibsonsCave-2048.jpg 2048w',
-        '/photos/GibsonsCave-1024.jpg 1024w',
-        '/photos/GibsonsCave-512.jpg 512w'
-    ], width: 6288, height: 4197, caption: {title: 'Gibson\'s Cave', content: 'Waterfall at Gibson\'s Cave.'}},
-    {src: '/photos/TeesValley.jpg', srcSet: [
-        '/photos/TeesValley-2048.jpg 2048w',
-        '/photos/TeesValley-1024.jpg 1024w',
-        '/photos/TeesValley-512.jpg 512w'
-    ], width: 6140, height: 4912,
-    caption: {title: 'Tees Valley', content: 'Evening over Tees Valley.'}},
-    {src: '/photos/Tree.jpg', srcSet: [
-        '/photos/Tree-2048.jpg 2048w',
-        '/photos/Tree-1024.jpg 1024w',
-        '/photos/Tree-512.jpg 512w'
-    ], width: 5214, height: 3477,
-    caption: {title: 'Tree', content: 'A chance moment, driving home I passed this tree. The way the light struck the tree struck me, and the beautiful cloud structure completed the scene.'}},
-    {src: '/photos/HayBale.jpg', srcSet: [
-        '/photos/HayBale-2048.jpg 2048w',
-        '/photos/HayBale-1024.jpg 1024w',
-        '/photos/HayBale-512.jpg 512w'
-    ], width: 5034, height: 2388,
-    caption: {title: 'Hay Bale', content: 'Walking through the fields, the hay bales add to the scene. The shapes of the hay bales are a perfect contrast to the lines of the fields.'}},
-    {src: '/photos/Fields.jpg', srcSet: [
-        '/photos/Fields-2048.jpg 2048w',
-        '/photos/Fields-1024.jpg 1024w',
-        '/photos/Fields-512.jpg 512w'
-    ], width: 3771, height: 1964,
-    caption: {title: 'Fields', content: 'Something about the fields.'}},
-    {src: '/photos/NorwichCathedral.jpg', srcSet: [
-        '/photos/NorwichCathedral-2048.jpg 2048w',
-        '/photos/NorwichCathedral-1024.jpg 1024w',
-        '/photos/NorwichCathedral-512.jpg 512w'
-    ], width: 4824, height: 3216,
-    caption: {title: 'Norwich Cathedral', content: 'Cathedral\'s have the most magnificent architecture. I always love to look up and see the detail that goes into the ceilings, that often goes unseen.'}}
+    {
+        src: '/photos/Tranquility.jpg',
+        srcSet: [
+            '/photos/Tranquility-2048.jpg 2048w',
+            '/photos/Tranquility-1024.jpg 1024w',
+            '/photos/Tranquility-512.jpg 512w'
+        ],
+        width: 5544,
+        height: 3696,
+        caption: {
+            title: 'Tranquility',
+            content: 'When the sun sets over the sea, the world is so peaceful. The colours reflect the peacefulness.'
+        }
+    },
+    {
+        src: '/photos/Ghostly.jpg',
+        srcSet: [
+            '/photos/Ghostly-2048.jpg 2048w',
+            '/photos/Ghostly-1024.jpg 1024w',
+            '/photos/Ghostly-512.jpg 512w'
+        ],
+        width: 1809,
+        height: 2171,
+        caption: {
+            title: 'Ghostly Pillars',
+            content: '.'
+        }
+    },
+    {
+        src:'/photos/Bassenthwaite.jpg',
+        srcSet: [
+            '/photos/Bassenthwaite-2048.jpg 2048w',
+            '/photos/Bassenthwaite-1024.jpg 1024w',
+            '/photos/Bassenthwaite-512.jpg 512w'
+        ],
+        width: 7360,
+        height: 3969,
+        caption: {
+            title: 'Bassenthwaite',
+            content: '.'
+        }
+    },
+    {
+        src: '/photos/BelmontViaduct.jpg',
+        srcSet: [
+            '/photos/BelmontViaduct-2048.jpg 2048w',
+            '/photos/BelmontViaduct-1024.jpg 1024w',
+            '/photos/BelmontViaduct-512.jpg 512w'
+        ],
+        width: 7360,
+        height: 4912,
+        caption: {
+            title: 'Belmont Viaduct',
+            content: 'This magnificent viaduct towers over the river weir. In the middle of the natural landscape, the man made monument compliments it well.'
+        }
+    },
+    {
+        src: '/photos/HighForce2.jpg',
+        srcSet: [
+            '/photos/HighForce2-2048.jpg 2048w',
+            '/photos/HighForce2-1024.jpg 1024w',
+            '/photos/HighForce2-512.jpg 512w'
+        ],
+        width: 7360,
+        height: 4912,
+        caption: {
+            title: 'High Force',
+            content: 'The power of High Force is always awe-inspiring. When you freeze it to just a moment, the movement of the water looks so graceful, and far from the loud, rushing waterfall.'
+        }
+    },
+    {
+        src: '/photos/LowForce.jpg',
+        srcSet: [
+            '/photos/LowForce-2048.jpg 2048w',
+            '/photos/LowForce-1024.jpg 1024w',
+            '/photos/LowForce-512.jpg 512w'
+        ],
+        width: 7360,
+        height: 4912,
+        caption: {
+            title: 'Low Force',
+            content: 'The waterfalls at low force.'
+        }
+    },
+    {
+        src: '/photos/GibsonsCave.jpg',
+        srcSet: [
+            '/photos/GibsonsCave-2048.jpg 2048w',
+            '/photos/GibsonsCave-1024.jpg 1024w',
+            '/photos/GibsonsCave-512.jpg 512w'
+        ],
+        width: 6288,
+        height: 4197,
+        caption: {
+            title: 'Gibson\'s Cave',
+            content: 'Waterfall at Gibson\'s Cave.'
+        }
+    },
+    {
+        src: '/photos/TeesValley.jpg',
+        srcSet: [
+            '/photos/TeesValley-2048.jpg 2048w',
+            '/photos/TeesValley-1024.jpg 1024w',
+            '/photos/TeesValley-512.jpg 512w'
+        ],
+        width: 6140,
+        height: 4912,
+        caption: {
+            title: 'Tees Valley',
+            content: 'Evening over Tees Valley.'
+        }
+    },
+    {
+        src: '/photos/Tree.jpg',
+        srcSet: [
+            '/photos/Tree-2048.jpg 2048w',
+            '/photos/Tree-1024.jpg 1024w',
+            '/photos/Tree-512.jpg 512w'
+        ],
+        width: 5214,
+        height: 3477,
+        caption: {
+            title: 'Tree',
+            content: 'A chance moment, driving home I passed this tree. The way the light struck the tree struck me, and the beautiful cloud structure completed the scene.'
+        }
+    },
+    {
+        src: '/photos/HayBale.jpg',
+        srcSet: [
+            '/photos/HayBale-2048.jpg 2048w',
+            '/photos/HayBale-1024.jpg 1024w',
+            '/photos/HayBale-512.jpg 512w'
+        ],
+        width: 5034,
+        height: 2388,
+        caption: {
+            title: 'Hay Bale',
+            content: 'Walking through the fields, the hay bales add to the scene. The shapes of the hay bales are a perfect contrast to the lines of the fields.'
+        }
+    },
+    {
+        src: '/photos/Fields.jpg',
+        srcSet: [
+            '/photos/Fields-2048.jpg 2048w',
+            '/photos/Fields-1024.jpg 1024w',
+            '/photos/Fields-512.jpg 512w'
+        ],
+        width: 3771,
+        height: 1964,
+        caption: {
+            title: 'Fields',
+            content: 'Something about the fields.'
+        }
+    },
+    {
+        src: '/photos/NorwichCathedral.jpg',
+        srcSet: [
+            '/photos/NorwichCathedral-2048.jpg 2048w',
+            '/photos/NorwichCathedral-1024.jpg 1024w',
+            '/photos/NorwichCathedral-512.jpg 512w'
+        ],
+        width: 4824,
+        height: 3216,
+        caption: {
+            title: 'Norwich Cathedral',
+            content: 'Cathedral\'s have the most magnificent architecture. I always love to look up and see the detail that goes into the ceilings, that often goes unseen.'
+        }
+    }
 ];
 
 export default photos;
